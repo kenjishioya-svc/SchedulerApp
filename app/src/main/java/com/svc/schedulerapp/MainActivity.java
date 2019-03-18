51993908cb2ae8afe4c939f304dd5db951d2aa81
kenjishioya-svc <kenji.shioya2@svc.co.jp>
2019-03-18 15:59:25 +0900

    first commit for Scheduler

---
+++ app/src/main/java/com/svc/schedulerapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.svc.schedulerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
